2ead0ab50def9f4340f523aba62e170cbff12005
debuggingsisyphos <niclasbraeuer@gmail.com>
2023-10-04 00:22:24 +0200

    Advance resource system

--- ResourceManagement.gd
+++ ResourceManagement.gd
@@ -3,5 +3,11 @@ class_name ResourceManagement extends Node
 
 @export var economic_actors : Array
 
-func exchange_resource(actor1, actor2, resource1, resource2) -> void:
-	pass
+func exchange_resource(seller : ResourceOwner, buyer : ResourceOwner, sold_resource : Resources, payed_resource : Resources) -> void:
+	if seller.owns_sufficient_resources(sold_resource) and buyer.owns_sufficient_resources(payed_resource):
+		seller.give_resource(sold_resource)
+		seller.receive_resource(payed_resource)
+		buyer.give_resource(payed_resource)
+		buyer.receive_resource(sold_resource)
+	else:
+		print("Not sufficient resources, implement signal please")
--- ResourceOwner.gd
+++ ResourceOwner.gd
@@ -3,3 +3,23 @@ class_name ResourceOwner extends Node
 @export var owned_resources : Array
 # a new class for actors should be created, then the string will be replaced
 @export var actor_reference : String
+
+func owns_resource(resource : Resources) -> bool:
+	if owned_resources.has(resource): return true
+	else: return false
+
+func owns_sufficient_resources(resource : Resources) -> bool:
+	if owns_resource(resource):
+		if get_resource(resource.resource_name).amount >= resource.amount: return true
+	return false
+		
+func get_resource(resource_name : String) -> Resources:
+	for resource in owned_resources:
+		if resource.resource_name == resource_name: return resource
+	return null
+	
+func receive_resource(resource : Resources) -> void:
+	pass
+	
+func give_resource(resource : Resources) -> void:
+	pass
